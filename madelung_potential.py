996c082254431d4aa8bbf70a8fc0d6f5f028225f
Asif_rm <7361722+Asif-Iqbal-Bhatti@users.noreply.github.com>
2022-06-13 14:10:44 +0200

    Update madelung_potential.py

--- madelung_potential.py
+++ madelung_potential.py
@@ -2,20 +2,20 @@
 
 '''
 #-------------------------------------------------------------------
-Simple script to calculate the Madelung potential (MP)
-
-Author  : Asif Iqbal
-Created on  : 14/05/2020
-
-USAGE : python3 argv.sys[0] It will read the CONTCAR automatically.
-NB: NO warranty guarranteed whatsoever or even implied. The script reads
-CONTCAR file and compute the Madelung potential. It is better to read the final
-position after relaxation and associated charges.
----
-This is a simple script which assumes the charges on the atom with atomic charges.
-In real case it should be approximated with Mulliken charge or Bader charge analysis. 
-The charges can be appended to the last line in the CONTCAR file and the script 
-can be modified to read the charges on each atom and calculate the MP.
+#Simple script to calculate the Madelung potential (MP)
+#
+#Author      : Asif Iqbal -> @AIB_EM
+#Created on  : 14/05/2020
+#
+#USAGE : python3 argv.sys[0] It will read the CONTCAR automatically.
+#NB: NO warranty guarranteed whatsoever or even implied. The script reads
+#CONTCAR file and compute the Madelung potential. It is better to read the final
+#position after relaxation and associated charges.
+#---
+#This is a simple script which assumes the charges on the atom with atomic charges.
+#In real case it should be approximated with Mulliken charge or Bader charge analysis. 
+#The charges can be appended to the last line in the CONTCAR file and the script 
+#can be modified to read the charges on each atom and calculate the MP.
 #-------------------------------------------------------------------
 '''
 
@@ -108,7 +108,7 @@ print ("Charge Dictionary ->")
 print (at_nu)
 
 #-------------------- Multiplying the list with atomic numbers 
-#                           according to the POSCAR list format
+#                 according to the POSCAR list format
 for i in range( len(atoms_types) ):
 	for k in range( int(atom_list[i]) ):
 		charge.append(at_nu[i][1])
